e420dd7e5d7f5f46fb24d7d10cbdf2c54f45eba4
Duncan <bonsairobo@gmail.com>
2020-02-21 23:47:52 -0800

    impl Into<DotVoxData> for Lattice

--- src/lattice.rs
+++ src/lattice.rs
@@ -146,21 +146,46 @@ impl<T> Lattice<T> {
     }
 }
 
-impl<I: LatticeIndexer> Lattice<u32, I> {
-    pub fn from_vox_with_indexer(indexer: I, data: &DotVoxData, model_index: usize) -> Self {
-        let DotVoxData {
-            models,
-            palette,
-            ..
-        } = data;
+impl<I: LatticeIndexer> Into<DotVoxData> for Lattice<u8, I> {
+    fn into(self: Self) -> DotVoxData {
+        let size = self.extent.get_local_supremum();
+        // Voxel coordinates are limited to u8.
+        assert!(size.x <= std::u8::MAX as i32);
+        assert!(size.y <= std::u8::MAX as i32);
+        assert!(size.z <= std::u8::MAX as i32);
 
+        let size = dot_vox::Size { x: size.x as u32, y: size.y as u32, z: size.z as u32 };
+
+        let mut voxels = Vec::new();
+        for p in &self.extent {
+            let i = *self.get_local(&p);
+            voxels.push(dot_vox::Voxel { x: p.x as u8, y: p.y as u8, z: p.z as u8, i });
+        }
+
+        let model = dot_vox::Model {
+            size,
+            voxels,
+        };
+
+        DotVoxData {
+            version: 150,
+            models: vec![model],
+            palette: Vec::new(),
+            materials: Vec::new(),
+        }
+    }
+}
+
+impl<I: LatticeIndexer> Lattice<u8, I> {
+    pub fn from_vox_with_indexer(indexer: I, data: &DotVoxData, model_index: usize) -> Self {
+        let DotVoxData { models, .. } = data;
         let Model { size: Size { x, y, z }, voxels } = &models[model_index];
         let size = Point::new(*x as i32, *y as i32, *z as i32);
         let extent = Extent::from_min_and_local_supremum([0, 0, 0].into(), size);
         let mut lattice = Lattice::fill_with_indexer(indexer, extent, 0);
         for Voxel { x, y, z, i } in voxels.into_iter() {
             let point = [*x as i32, *y as i32, *z as i32].into();
-            *lattice.get_mut_local(&point) = palette[*i as usize];
+            *lattice.get_mut_local(&point) = *i;
         }
 
         lattice
