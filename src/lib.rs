2e59c1ffb32e2fcb2c8ce76019854f3732a4cda4
Duncan <bonsairobo@gmail.com>
2020-02-23 14:19:29 -0800

    add the 'vox' feature for dot_vox support

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,19 @@ mod lattice;
 mod normal;
 mod point;
 
+#[cfg(feature = "vox")]
+mod vox;
+
+#[cfg(feature = "vox")]
+pub use vox::{VoxColor, EMPTY_VOX_COLOR};
+
 #[cfg(test)]
 mod test_util;
 
 pub use extent::{bounding_extent, Extent, ExtentIterator};
 pub use lattice::{
     ChunkKeyIterator, ChunkedLattice, ChunkedLatticeIterator, Lattice, LatticeIndexer,
-    LatticeKeyValIterator, PeriodicYLevelsIndexer, VoxColor, YLevelsIndexer, EMPTY_VOX_COLOR,
+    LatticeKeyValIterator, PeriodicYLevelsIndexer, YLevelsIndexer,
 };
 pub use normal::{Direction, DirectionIndex, Normal, PlaneSpanInfo, ALL_DIRECTIONS, ALL_NORMALS};
 pub use point::Point;
