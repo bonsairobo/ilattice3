942ebc917c16d29ae6eb05265012ca89d2e4ea57
Duncan <bonsairobo@gmail.com>
2020-02-22 20:04:37 -0800

    export YLevelsIndexer

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod test_util;
 pub use extent::{bounding_extent, Extent, ExtentIterator};
 pub use lattice::{
     ChunkKeyIterator, ChunkedLattice, ChunkedLatticeIterator, Lattice, LatticeIndexer,
-    LatticeKeyValIterator, PeriodicYLevelsIndexer, VoxColor,
+    LatticeKeyValIterator, PeriodicYLevelsIndexer, VoxColor, YLevelsIndexer,
 };
 pub use normal::{Direction, DirectionIndex, Normal, PlaneSpanInfo, ALL_DIRECTIONS, ALL_NORMALS};
 pub use point::Point;
