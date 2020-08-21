f48f1bd661205fb14761b4f0d74af2168b269921
Duncan <bonsairobo@gmail.com>
2020-08-21 01:56:41 -0700

    add some geometric Point methods

--- src/point.rs
+++ src/point.rs
@@ -111,6 +111,18 @@ impl Point {
         self.x * other.x + self.y * other.y + self.z * other.z
     }
 
+    pub fn cross(&self, other: &Self) -> Self {
+        Point::new(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x,
+        )
+    }
+
+    pub fn norm_squared(&self) -> i32 {
+        self.dot(self)
+    }
+
     pub fn squared_distance(&self, other: &Self) -> i32 {
         let diff = *self - *other;
 
